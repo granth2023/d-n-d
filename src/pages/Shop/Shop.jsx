9c9cc6d4f0cf4d0560fa31178031c32dcc3019c3
granth2023 <grantharris38@gmail.com>
2024-01-11 10:53:22 -0500

    usestate

--- src/pages/Shop/Shop.jsx
+++ src/pages/Shop/Shop.jsx
@@ -1,10 +1,13 @@
-import React from "react"
+
+import { useState } from 'react'
 import InventoryList from "../../components/InventoryList/InventoryList"
 import { inventoryData } from "../../data/data"
 
 
 const Shop = () => {
     console.log('Inventory Data:', inventoryData)
+
+    const [inventory, setInventory] = useState(inventoryData)
     return ( 
         <main> 
             <h1>Shop</h1>
@@ -16,3 +19,9 @@ const Shop = () => {
 export default Shop
 
 //are props immutable? // can you say props.prop equals something else? how do props work? 
+
+//how do you use hooks? use state is a hook? will we have other hooks? 
+
+//how do we destructure usestate? is that extracting? are we plucking it from the react library? 
+
+//how do we do this? what is the first hting in the array for state? what is the first? waht is the second? the name and the word set? what is this saying? what is equal to? do you call usestate like a funciotn? what do you pass? is it inventory Data? what are we technically doing? 
