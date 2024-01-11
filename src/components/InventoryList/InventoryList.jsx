42188862d97adda4d575c2091034604ea9385392
granth2023 <grantharris38@gmail.com>
2024-01-11 13:32:21 -0500

    handle item buttons added to inventory

--- src/components/InventoryList/InventoryList.jsx
+++ src/components/InventoryList/InventoryList.jsx
@@ -1,17 +1,24 @@
-const InventoryList = (props) => { 
+const InventoryList = (props) => {
     return (
-        <div>
-            <h2> {props.title}</h2>
-            <ul>
-                {props.inventory.map((item) => (
-                    <li key={item._id}>
-                        <p>{item.name}</p>
-                        <p>Cost: {item.cost}</p>
-                    </li>
-                ))}
-            </ul>
-            </div>
+      <div>
+        <h2>{props.title}</h2>
+        <ul>
+          {props.inventory.map((item) => (
+            <li key={item._id}>
+              <p>{item.name}</p>
+              <p>Cost: {item.cost}</p>
+  
+              {props.handleAddItem
+                ? <button>Add Item</button>
+                : <button>Remove Item</button>
+              }
+  
+            </li>
+          ))}
+        </ul>
+      </div>
     )
-}
+  }
+  
+  export default InventoryList
 
-export default InventoryList
