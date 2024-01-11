ce8833dc1fe80b98ecabda73e645803e409b8d14
granth2023 <grantharris38@gmail.com>
2024-01-11 13:29:43 -0500

    shop css

--- src/pages/Shop/Shop.jsx
+++ src/pages/Shop/Shop.jsx
@@ -8,10 +8,20 @@ const Shop = () => {
     console.log('Inventory Data:', inventoryData)
 
     const [inventory, setInventory] = useState(inventoryData)
+    const [userInventory, setUserInventory] = useState([])
+
+
+    const handleAddItem = () => {
+        setInventory([])
+    }
     return ( 
         <main> 
             <h1>Shop</h1>
-           <InventoryList inventory={inventoryData}/>
+            <section>
+            <button onClick={handleAddItem}>Click Here</button>
+           <InventoryList title="Shop Inventory" inventory={inventoryData}/>
+           <Inventory List title="User Inventory" inventory={userInventory}/>
+           </section>
         </main>
     )
 }
@@ -25,3 +35,5 @@ export default Shop
 //how do we destructure usestate? is that extracting? are we plucking it from the react library? 
 
 //how do we do this? what is the first hting in the array for state? what is the first? waht is the second? the name and the word set? what is this saying? what is equal to? do you call usestate like a funciotn? what do you pass? is it inventory Data? what are we technically doing? 
+
+//when you pass things into the use state what will be the first thing in the array? 
