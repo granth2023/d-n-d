f3f91eacac98e3938d3312eec32695bc0c538306
granth2023 <grantharris38@gmail.com>
2024-01-11 09:52:59 -0500

    opened up via app

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,18 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState } from 'react';
+
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+//components and pages
+import Shop from './pages/Shop/Shop';
 
+const App = () => {
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <main class
+     <Shop />
   )
 }
 
-export default App
 
+  export default App 
 
-//when i open app, think what are my components? can we create one component and reuse? can we define a list once and use it for two different functions? the store and user? 
-
-//what makes them the same? the fact they have a list structure and use similar components within functions
-//how do we create the shop component? 
+  //what is a reservered keyword? return? calss? 
