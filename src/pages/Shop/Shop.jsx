6d1a4aaafc7e875498175435438a27aee02ec1a4
granth2023 <grantharris38@gmail.com>
2024-01-11 10:04:05 -0500

    list appears

--- src/pages/Shop/Shop.jsx
+++ src/pages/Shop/Shop.jsx
@@ -6,6 +6,15 @@ const Shop = () => {
     return ( 
         <main> 
             <h1>Shop</h1>
+            <ul>
+                {inventoryData.map((item) => (
+                    <li key={item._id}>
+                        <p>{item.name}</p>
+                        <p>Cost: {item.cost}</p>
+                        </li>
+               ))}
+
+            </ul>
         </main>
     )
 }
